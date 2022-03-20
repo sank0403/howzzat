8087f51e628d4a5cd7fafe056e00897571b0a2ca
sank0403 <58062842+sank0403@users.noreply.github.com>
2022-03-20 06:21:42 -0400

    Add files via upload

---
+++ howzzat.js
@@ -0,0 +1,390 @@
+let i = 40;
+window.onload = onTimer();
+function onTimer() {
+  document.getElementById('mycounter').innerHTML = i;
+  i--;
+  if (i < 0)  {
+	if (!gameOver){  
+		document.getElementById("answer").style.color = "red";
+		document.getElementById("answer").innerText = "Time is up! You lose!\n The player was "+firstname+" "+lastname;
+		gameOver = true;
+	}
+  }
+  else {
+	setTimeout(onTimer, 1000);
+  }
+}
+
+
+setTimeout(FetchData1, 10000)
+	function FetchData1(){
+		if (!gameOver){
+		document.getElementById(0).innerText = year;
+		}
+	}	
+
+setTimeout(FetchData2, 20000)
+	function FetchData2(){
+		if (!gameOver){
+		document.getElementById(1).innerText = country;
+		}
+	}
+
+setTimeout(FetchData3, 30000)
+	function FetchData3(){
+		if (!gameOver){
+		document.getElementById(2).innerText = catg;
+		}
+	}
+
+// *****************************************************************************
+
+var gameOver = false;
+//console.log(gameType);
+var yearList = ["1975","1975","1975","1975","1979","1979","1979","1979","1983","1983","1983","1983","1987","1987","1987","1987","1992","1992","1992","1992","1992","1996","1996","1996","1996","1996","1999","1999","1999","1999","1999","1999","2003","2003","2003","2003","2003","2007","2007","2007","2007","2007","2011","2011","2011","2011","2011","2011","2015","2015","2015","2015","2015","2015","2019","2019","2019","2019","2019"]
+var countryList = ["WIN","NZL","AUS","WIN","WIN","WIN","ENG","WIN","IND","ENG","IND","IND","AUS","ENG","AUS","AUS","PAK","NZL","PAK","PAK","NZL","SRL","IND","IND","SRL","SRL","AUS","IND","AUS","NZL","AUS","SAF","AUS","IND","SRL","AUS","IND","AUS","AUS","AUS","AUS","AUS","IND","SRL","PAK","IND","IND","IND","AUS","NZL","AUS","NZL","AUS","AUS","ENG","IND","AUS","ENG","NZL",] 
+var catgTypeList = ["WC","MR","MW","MF","WC","MR","MW","MF","WC","MR","MW","MF","WC","MR","MW","MF","WC","MR","MW","MF","MS","WC","MR","MW","MF","MS","WC","MR","MW","MW","MF","MS","WC","MR","MW","MF","MS","WC","MR","MW","MF","MS","WC","MR","MW","MW","MF","MS","WC","MR","MW","MW","MF","MS","WC","MR","MW","MF","MS",]
+var firstnameList = ["Clive","Glenn","Gary","Clive","Clive","Gordon","Mike","Vivian","Kapil","David","Roger","Mohinder","Allan","Graham","Craig","David","Imran","Martin","Wasim","Wasim","Martin","Arjuna","Sachin","Anil","Aravinda","Sanath","Steve","Rahul","Shane","Geoff","Shane","Lance","Ricky","Sachin","Chaminda","Ricky","Sachin","Ricky","Matthew","Glenn","Adam","Glenn","Mahendra","Tillakaratne","Shahid","Zaheer","Mahendra","Yuvraj","Michael","Martin","Mitchell","Trent","James","Mitchell","Eoin","Rohit","Mitchell","Ben","Kane",]
+var lastnameList = ["Lloyd","Turner","Gilmour","Lloyd","Lloyd","Greenidge","Hendrick","Richards","Dev","Gower","Binny","Amarnath","Border","Gooch","McDermott","Boon","Khan","Crowe","Akram","Akram","Crowe","Ranatunga","Tendulkar","Kumble","de Silva","Jayasuriya","Waugh","Dravid","Warne","Allott","Warne","Klusener","Ponting","Tendulkar","Vaas","Ponting","Tendulkar","Ponting","Hayden","McGrath","Gilchrist","McGrath","Singh Dhoni","Dilshan","Afridi","Khan","Singh Dhoni","Singh","Clarke","Guptill","Starc","Boult","Faulkner","Starc","Morgan","Sharma","Starc","Stokes","Williamson",]
+var index = [Math.floor(Math.random()*yearList.length)];
+var firstname = firstnameList[index];
+var lastname = lastnameList[index];
+var year = yearList[index];
+var catg = catgTypeList[index];
+var country = countryList[index];
+var fnwidth = firstname.length;
+var lnwidth = lastname.length;
+var width = fnwidth + lnwidth;
+var answername = "";
+answername = firstname.concat(lastname).toLowerCase();
+console.log(firstname);
+console.log(lastname);
+
+document.getElementById("answer").style.color = "cyan";
+document.getElementById("answer").innerText = "Read the Game Rules to learn how to play.\n Timer in Progress - Refresh Page to reload!";
+var row = 0; //current guess (attempt #)
+var col = 0; //current letter for that attempt
+
+//var boardWidth = (width*30); 
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+window.onload = function(){
+	//console.log("sankar");
+    intialize();
+}
+
+
+function intialize() {
+
+    // Create the game board
+/* 	
+        for (let clueindex = 0; clueindex < 3; clueindex++) {
+            // <span id="0-0" class="tile">P</span>
+            let clue = document.createElement("span");
+            clue.id = clueindex;
+            clue.classList.add("clue-item");
+			if (clueindex == 0){	
+            clue.innerText = "Year";
+			}
+			else if (clueindex == 1){	
+            clue.innerText = "Country";
+			}
+			else if (clueindex == 2){	
+            clue.innerText = "Category";
+			}			
+            document.getElementById("clue-item").appendChild(clue);
+        } */
+
+        for (let clueindex = 0; clueindex < 3; clueindex++) {
+            // <span id="0-0" class="tile">P</span>
+            let clue = document.createElement("span");
+            clue.id = clueindex;
+            clue.classList.add("clue-ball");
+			if (clueindex == 0){	
+            clue.innerHTML = '<img class="image" src="ball.png" alt="Alt text"/><div class="centered">Year</div>';
+			}
+			else if (clueindex == 1){	
+            clue.innerHTML = '<img class="image" src="ball.png" alt="Alt text"/><div class="centered">Country</div>';
+			}
+			else if (clueindex == 2){	
+            clue.innerHTML = '<img class="image" src="ball.png" alt="Alt text"/><div class="centered">Category</div>';
+			}				
+            document.getElementById("clue-ball").appendChild(clue);
+        }	
+		
+
+
+   // for (let r = 0; r < height; r++) {
+	    let f = 1
+        for (let c = 0; c < fnwidth; c++) {
+            // <span id="0-0" class="tile">P</span>
+            let tile = document.createElement("span");
+            tile.id = f.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.innerText = "";
+            document.getElementById("boardfirst").appendChild(tile);
+        }
+    //}
+  
+	    let l = 1
+        for (let c = fnwidth; c < width; c++) {
+            // <span id="0-0" class="tile">P</span>
+            let tile = document.createElement("span");
+            tile.id = l.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.innerText = "";
+            document.getElementById("boardlast").appendChild(tile);
+        }
+    //}	
+	
+
+    // Create the key board
+    let keyboard = [
+        ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+        ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫" ]
+    ]
+	
+/*     let keyboard = [
+        ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", " "],
+        ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫" ]
+    ]	 */
+
+    for (let i = 0; i < keyboard.length; i++) {
+        let currRow = keyboard[i];
+        let keyboardRow = document.createElement("div");
+        keyboardRow.classList.add("keyboard-row");
+
+        for (let j = 0; j < currRow.length; j++) {
+            let keyTile = document.createElement("div");
+
+            let key = currRow[j];
+            keyTile.innerText = key;
+            if (key == "Enter") {
+                keyTile.id = "Enter";
+            }
+            else if (key == "⌫") {
+                keyTile.id = "Backspace";
+            }
+            else if ("A" <= key && key <= "Z") {
+                keyTile.id = "Key" + key; // "Key" + "A";
+            } 
+
+            keyTile.addEventListener("click", processKey);
+
+            if (key == "Enter") {
+                keyTile.classList.add("enter-key-tile");
+            } else {
+                keyTile.classList.add("key-tile");
+            }
+            keyboardRow.appendChild(keyTile);
+        }
+        document.body.appendChild(keyboardRow);
+    }
+    
+
+    // Listen for Key Press
+    document.addEventListener("keyup", (e) => {
+        processInput(e);
+    })
+}
+
+
+function processKey() {
+    e = { "code" : this.id };
+    processInput(e);
+}
+
+function processInput(e) {
+    if (gameOver) return; 
+
+    // alert(e.code);
+    if ("KeyA" <= e.code && e.code <= "KeyZ") {
+        if (col < width) {
+            let currTile = document.getElementById(1 + '-' + col.toString());
+            if (currTile.innerText == "") {
+                currTile.innerText = e.code[3];
+                col += 1;
+            }
+        }
+		document.getElementById("answer").innerText = "";
+    }
+    else if (e.code == "Backspace") {
+        if (0 < col && col <= width) {
+            col -=1;
+        }
+        let currTile = document.getElementById(1 + '-' + col.toString());
+        currTile.innerText = "";
+		document.getElementById("answer").innerText = "";
+    }
+
+    else if (e.code == "Enter") {
+        update();
+    }
+
+/*     if (!gameOver && row == height) {
+        gameOver = true;
+		document.getElementById("answer").style.color = "red";
+        document.getElementById("answer").innerText = "The  " +gameName +"  was " + word + "! Try Again.";
+    } */
+}
+
+function update() {
+    let guess = "";
+    document.getElementById("answer").innerText = "";
+
+    //string up the guesses into the word
+    for (let c = 0; c < width; c++) {
+        let currTile = document.getElementById(1 + '-' + c.toString());
+        let letter = currTile.innerText;
+        guess += letter;
+    }
+
+    guess = guess.toLowerCase(); //case sensitive
+	
+/*     if (gameMode == 1){
+		if (!gameOver && col < width) {
+			document.getElementById("answer").style.color = "red";
+			document.getElementById("answer").innerText = "Not enough letters!";
+			return;
+		}   
+	} */		
+
+/* 	if (!guessList.includes(guess)) {
+		document.getElementById("answer").style.color = "red";
+		document.getElementById("answer").innerText = "Not a valid  " +gameName +"  name!";
+		return;
+	}	 */
+
+	
+	if (guess == answername) {
+		document.getElementById("answer").style.color = "green";
+		document.getElementById("answer").innerText = "You have identified the player successfully.\nCongratulations! Time Taken is " + (40-i) + " seconds";
+		gameOver = true;
+		var hidecounter = document.getElementById("mycounter");
+		hidecounter.style.color = "black";
+	}	
+	else{
+		document.getElementById("answer").style.color = "red";
+		document.getElementById("answer").innerText = "Incorrect Guess! \n Please try again.";
+		for (let c = col-1; c >= 0; c--) {
+			let currTile = document.getElementById(1 + '-' + c.toString());
+			currTile.innerText = "";
+			col -= 1;
+		}		
+	}
+
+    //start processing guess
+ /*    let correct = 0;
+
+    let letterCount = {}; //keep track of letter frequency, ex) KENNY -> {K:1, E:1, N:2, Y: 1}
+    for (let i = 0; i < word.length; i++) {
+        let letter = word[i];
+
+        if (letterCount[letter]) {
+           letterCount[letter] += 1;
+        } 
+        else {
+           letterCount[letter] = 1;
+        }
+    }
+
+    //first iteration, check all the correct ones first
+    for (let c = 0; c < width; c++) {
+        let currTile = document.getElementById(row.toString() + '-' + c.toString());
+        let letter = currTile.innerText;
+		//Is it in the correct position?
+		if (word[c] == letter) {
+			currTile.classList.add("correct");
+		//disable hint button if first letter is correct 			
+			if (c == 0) {
+				document.getElementById("hint").disabled = true;
+				if (hintSwitch == 0){
+				document.getElementById("answer").style.color = "cyan";
+				document.getElementById("answer").innerText = "You have identified the First Letter.\nHint is now disabled.";	
+				}	
+			}
+			let keyTile = document.getElementById("Key" + letter);
+			keyTile.classList.remove("present");
+			keyTile.classList.add("correct");
+
+			correct += 1;
+			letterCount[letter] -= 1; //deduct the letter count
+		}
+
+	
+		if (correct == width) {
+			document.getElementById("answer").style.color = "green";
+			document.getElementById("answer").innerText = "Congratulations! \n You have identified the " +gameName +" successfully.";
+			gameOver = true;
+		}
+    }
+
+    //go again and mark which ones are present but in wrong position
+    for (let c = 0; c < width; c++) {
+        let currTile = document.getElementById(row.toString() + '-' + c.toString());
+        let letter = currTile.innerText;
+
+        // skip the letter if it has been marked correct
+        if (!currTile.classList.contains("correct")) {
+            //Is it in the word?         //make sure we don't double count
+            if (word.includes(letter) && letterCount[letter] > 0) {
+                currTile.classList.add("present");
+                
+                let keyTile = document.getElementById("Key" + letter);
+                if (!keyTile.classList.contains("correct")) {
+                    keyTile.classList.add("present");
+                }
+                letterCount[letter] -= 1;
+            } // Not in the word or (was in word but letters all used up to avoid overcount)
+            else {
+                currTile.classList.add("absent");
+                let keyTile = document.getElementById("Key" + letter);
+				if (keyTile !== null){
+					if (!keyTile.classList.contains("present") && !keyTile.classList.contains("correct")) {
+						keyTile.classList.add("absent");
+					}	
+				}
+            }
+        }
+    }
+
+    row += 1; //start new row
+    col = 0; //start at 0 for new row */
+}
